c80ea7defbb3aa23b604ddf56e0a965e1c28c95e
preston <prestonolds@gatech.edu>
2017-11-28 10:41:25 -0500

    Tasks have date picker and are clickable

--- HEADS_UP/app/src/main/java/com/example/weichen/jd_injuryprecaution_prototype/Task.java
+++ HEADS_UP/app/src/main/java/com/example/weichen/jd_injuryprecaution_prototype/Task.java
@@ -1,10 +1,17 @@
 package com.example.weichen.jd_injuryprecaution_prototype;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.weichen.jd_injuryprecaution_prototype.db.Group_db;
+import com.example.weichen.jd_injuryprecaution_prototype.db.Task_db;
+import com.example.weichen.jd_injuryprecaution_prototype.db.Task_dbHelper;
 
 /**
  * Created by preston on 11/12/17.
@@ -17,8 +24,17 @@ public class Task extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
+        final Intent intent = getIntent();
+        String[] task_info = intent.getStringExtra("task_info").split(" - ");
+
+        TextView tvDateContent = (TextView) findViewById(R.id.tv_date_content);
+        TextView tvTypeContent = (TextView) findViewById(R.id.tv_type_content);
+
         Button bCancel = (Button) findViewById(R.id.bCancel);
         Button bComplete = (Button) findViewById(R.id.bComplete);
+
+        tvDateContent.setText(task_info[0]);
+        tvTypeContent.setText(task_info[1]);
 
         bCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +46,13 @@ public class Task extends AppCompatActivity {
         bComplete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Task_dbHelper mHelper = new Task_dbHelper(getBaseContext());
+
+                SQLiteDatabase db = mHelper.getWritableDatabase();
+                db.delete(Group_db.GroupEntry.TABLE, Task_db.TaskEntry.COL_TASK_DATE + " = ?", new String[] {intent.getStringExtra("task_info")});
+                db.close();
+
                 startActivity(new Intent(Task.this, Activities.class));
             }
         });
